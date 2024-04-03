940942d058fb7873d3dcbc962003a8de2a521155
Vinicius Benicio <vinicius.benicio97@gmail.com>
2024-04-02 23:06:05 -0300

    fix validation maximum attendees

--- PassIn.Application/UseCases/Events/RegisterAttendee/RegisterAttedeeOnEventUseCase.cs
+++ PassIn.Application/UseCases/Events/RegisterAttendee/RegisterAttedeeOnEventUseCase.cs
@@ -55,7 +55,7 @@ namespace PassIn.Application.UseCases.Events.RegisterAttendee
                 throw new ConflictException("You can not register twice on the same event");
 
             var attendeesForEvent = _dbContext.Attendees.Count(attendee => attendee.Event_Id == eventId);
-            if (attendeesForEvent > eventEntity.Maximum_Attendees)
+            if (attendeesForEvent == eventEntity.Maximum_Attendees)
                 throw new ErrorOnValidationException("There is no room for this event.");
         }
 
